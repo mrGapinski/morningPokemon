ed3fffef97b248c507349fc7d4409c23fbf5d635
JKraus <JKraus@10.0.0.14>
2020-05-26 08:30:29 -0500

    Added print commands.

--- src/test2/BenDebug.java
+++ src/test2/BenDebug.java
@@ -4,7 +4,8 @@ public class BenDebug {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("test");
+System.out.println("another test");
 	}
 
 }
