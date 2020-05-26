f4daa8e4ed1c2d960df6c74b0b9e79dd72613feb
lukyc <lukyc@LAPTOP-KUO6MJ4H>
2020-05-26 17:06:58 -0500

    Deleted the extra LukeDebug.java class

--- src/test2/LukeDebug.java
+++ src/test2/LukeDebug.java
@@ -2,4 +2,9 @@ package test2;
 
 public class LukeDebug {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hiya there! I don't know what else to say so yeah, ha");
+	}
+
 }
