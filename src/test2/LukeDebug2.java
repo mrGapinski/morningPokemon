482c096f1a2dd914d478082ac3e64d27a3da4439
lukyc <lukyc@LAPTOP-KUO6MJ4H>
2020-05-26 17:00:40 -0500

    added some words

--- src/test2/LukeDebug2.java
+++ src/test2/LukeDebug2.java
@@ -4,7 +4,7 @@ public class LukeDebug2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hiya there! I don't know what else to say so yeah");
+		System.out.println("Hiya there! I don't know what else to say so yeah, ha");
 	}
 
 }
